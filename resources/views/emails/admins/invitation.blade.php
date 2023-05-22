14d77a793db5a6d94d292997b07b9171616389e7
Vahagn <vahagn99ghukasyan@gmail.com>
2023-05-23 02:42:00 +0400

    init

--- resources/views/emails/admins/invitation.blade.php
+++ resources/views/emails/admins/invitation.blade.php
@@ -1,14 +1,10 @@
-<x-mail::message>
-    # Հրավեր
-
-    Դուք հրավերված եք մեր կայքին միանալուն։
-
-    <x-mail::button :url="$registrationLink">
-        Ջեր ներկաիս գաղտնաբառն է "password" դուք կարող եք փոխել այն մուտք գործելուց հետո։
-        Խնդրում եմ սեղմել ներքևի հղումը ձեր գրանցումը ավարտելու համար՝
-
-    </x-mail::button>
-
-    Thanks,<br>
+@component('mail::message')
+    # Բարև {{  $name }}
+    Ջեր ներկայիս գաղտնաբառն է "password" դուք կարող եք փոխել այն մուտք գործելուց հետո։
+    Խնդրում եմ սեղմել ներքևի հղումը ձեր գրանցումը ավարտելու համար՝
+    @component('mail::button', ['url' => $registrationLink])
+        Հաստատել
+    @endcomponent
+    Շնորհակալություն,<br>
     {{ config('app.name') }}
-</x-mail::message>
+@endcomponent
